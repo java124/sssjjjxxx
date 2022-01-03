7071485a443d1742b62d95398c5fe31059b7d482
java124 <95834369+java124@users.noreply.github.com>
2022-01-03 19:47:15 +0800

    第一次代码作业——一二章例题

---
+++ SalesTax.java
@@ -0,0 +1,13 @@
+package ex1;
+import java.util.Scanner;
+public class SalesTax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter purchase amout: ");
+		double purchaseAmount = input.nextDouble();
+		double tax = purchaseAmount *0.06;
+		System.out.println("Sales tax  is $"+(int)(tax *100)/100.0);
+	}
+}
